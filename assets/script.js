1bf3be6e309b3b50935e74bde7164ea1f51a03a3
Burke <beatty.burke@gmail.com>
2021-04-28 13:59:05 -0400

    background color function and removed first attempt at local storage

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,8 @@ $('#currentDay').text(today.format('MMMM Do YYYY, LT'));
 setInterval(function(){
     $('#currentDay').text(today.format('MMMM Do YYYY, LT'))}, 1000)
 
+const rows = document.getElementsByClassName("row");
+var currentHour = parseInt(moment().format('H'));
 
 Array.from(rows).forEach(row => {
     let
@@ -28,23 +30,4 @@ Array.from(rows).forEach(row => {
 
 function setColor(element, color) {
     element.style.backgroundColor = color;
-}
-// local storage
-
-$('#textarea1').val(localStorage.textarea1)
-$('#textarea2').val(localStorage.textarea2)
-$('#textarea3').val(localStorage.textarea3)
-$('#textarea4').val(localStorage.textarea4)
-$('#textarea5').val(localStorage.textarea5)
-$('#textarea6').val(localStorage.textarea6)
-$('#textarea7').val(localStorage.textarea7)
-$('#textarea8').val(localStorage.textarea8)
-$('#textarea9').val(localStorage.textarea9)
-
-
-// background color changes
-function timeColor(){
-    var time = new Date().getHours();
-    console.log(time);
-
 }
