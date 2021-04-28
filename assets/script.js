c33332cfa2b39c207e1c198821054ad07e058e10
Burke <beatty.burke@gmail.com>
2021-04-28 13:43:23 -0400

    background color function with html ids

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,29 @@ setInterval(function(){
     $('#currentDay').text(today.format('MMMM Do YYYY, LT'))}, 1000)
 
 
+Array.from(rows).forEach(row => {
+    let
+        rowIdString = row.id,
+        rowHour;
+    if (rowIdString) {
+    rowHour = parseInt(rowIdString);
+    }
+    if (rowHour) {
+        if (currentHour === rowHour) {
+            setColor(row, 'red');
+        } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
+            setColor(row, "green");
+        } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
+            setColor(row, "lightgrey");
+        } else {
+            setColor(row, "white");
+        }
+    }
+});
+
+function setColor(element, color) {
+    element.style.backgroundColor = color;
+}
 // local storage
 
 $('#textarea1').val(localStorage.textarea1)
@@ -17,3 +40,11 @@ $('#textarea6').val(localStorage.textarea6)
 $('#textarea7').val(localStorage.textarea7)
 $('#textarea8').val(localStorage.textarea8)
 $('#textarea9').val(localStorage.textarea9)
+
+
+// background color changes
+function timeColor(){
+    var time = new Date().getHours();
+    console.log(time);
+
+}
